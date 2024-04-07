977e633f3bc4e3eb7a5159a6b09b2b175f96cb39
HNT1807 <121518539+HNT1807@users.noreply.github.com>
2024-04-07 15:00:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-import streamlit as st
 import ollama
+import streamlit as st
 
-# Streamlit app to input filenames and identify music stems
-def main():
-    st.title('Music Stem Identifier')
+st.title("Ollama Python Chatbot")
 
-    # Text area for user input
-    user_input = st.text_area("Enter the music file names, each one on a new line:", "")
+# initialize history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-    # Button to run the Ollama model
-    if st.button('Identify Stems'):
-        if user_input:  # Check if the user has entered any text
-            filenames = user_input.split('\n')  # Split the input by new lines into a list
-            filenames = [name.strip() for name in filenames if name.strip()]  # Remove any extra whitespace
+# init models
+if "model" not in st.session_state:
+    st.session_state["model"] = ""
 
-            prompt = f"Given these music file names, identify the specific stem each represents. \
-            Format your response by pairing the file name with the stem, without altering the original stem name. \
-            Here are the files: {filenames}. Make sure to format the answer with only filename = instrument(s) you identified, don't write anything else. Your guess must be included in the filename, like don't come up with an instrument that is not in the filename"
+models = [model["name"] for model in ollama.list()["models"]]
+st.session_state["model"] = st.selectbox("Choose your model", models)
 
-            # Call the ollama generate function
-            output = ollama.generate(model="mistral", prompt=prompt, stream=True)
+def model_res_generator():
+    stream = ollama.chat(
+        model=st.session_state["model"],
+        messages=st.session_state["messages"],
+        stream=True,
+    )
+    for chunk in stream:
+        yield chunk["message"]["content"]
 
-            # Concatenate and display the responses as a single block
-            full_response = ''.join(chunk["response"] for chunk in output)
-            st.text(full_response)
-        else:
-            st.error("Please enter at least one filename.")
+# Display chat messages from history on app rerun
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-if __name__ == '__main__':
-    main()
+if prompt := st.chat_input("What is up?"):
+    # add latest message to history in format {role, content}
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message = st.write_stream(model_res_generator())
+        st.session_state["messages"].append({"role": "assistant", "content": message})
